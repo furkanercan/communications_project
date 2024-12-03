b2d69058d42ff002ec7140752584f426b2bda548
Furkan Ercan <furkanercan88@gmail.com>
2024-12-03 13:37:44 -0500

    add src as a symbolic link to tests

--- setup.sh
+++ setup.sh
@@ -5,3 +5,11 @@ export PYTHONPATH=$(pwd)/src:$PYTHONPATH
 
 # Optionally, display the new PYTHONPATH
 echo "PYTHONPATH set to: $PYTHONPATH"
+
+        # chmod +x ./setup.sh  # Make sure setup.sh is executable
+        # ./setup.sh           # Run the script to set PYTHONPATH
+
+# Set a symbolic link to tests folder
+ln -s src tests/src
+# Verify the symbolic link
+ls -l tests/src
